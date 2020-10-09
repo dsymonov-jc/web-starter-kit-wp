fd4c36696db37483e1acc430b6d4578e051bc6f3
dsymonov <dsymonov@justcoded.co>
2020-10-09 15:37:55 +0300

    rebuild task lint for eslint new api & update config

--- tasks/lint-js.js
+++ tasks/lint-js.js
@@ -3,24 +3,26 @@
  */
 'use strict';
 
-const CLIEngine = require('eslint').CLIEngine;
+const ESLint = require('eslint').ESLint;
 
 const global = require('../gulp-config.js');
 
 module.exports = function (options) {
   const fixJs = global.isFixJs();
-  const cli = new CLIEngine({
+  const eslint = new ESLint({
     fix: fixJs,
     useEslintrc: true,
   });
 
-  return (done) => {
-    const report = cli.executeOnFiles(['./js/**/*.js']);
-    const formatter = cli.getFormatter();
+  return async (done) => {
+    const results = await eslint.lintFiles(['./js/**/*.js']);
 
-    if (formatter(report.results) !== '') console.log(formatter(report.results));
+    if (fixJs) await ESLint.outputFixes(results);
 
-    CLIEngine.outputFixes(report);
+    const formatter = await eslint.loadFormatter('stylish');
+    const resultText = formatter.format(results);
+
+    if (resultText !== '') console.log(resultText);
 
     return done();
   };
