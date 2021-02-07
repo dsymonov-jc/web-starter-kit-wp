6e52ba623aad1aaed97350c3e8aa1f0c4c30f4b0
Kassaila <kasela.kassaila@gmail.com>
2021-02-07 13:46:54 +0200

    update htmlhint

--- tasks/lint-html.js
+++ tasks/lint-html.js
@@ -5,26 +5,12 @@
 const gulp = require('gulp');
 const htmlhint = require('gulp-htmlhint');
 
-const notifier = require('../helpers/notifier');
 const global = require('../gulp-config.js');
 
 module.exports = function () {
-  return (done) => {
+  return () =>
     gulp
       .src(`../${global.folder.build}/**/*.html`)
-      .pipe(
-        htmlhint({
-          'attr-lowercase': false,
-        })
-      )
-      .pipe(htmlhint.reporter('htmlhint-stylish'))
-      .pipe(
-        htmlhint.failOnError({
-          suppress: true,
-        })
-      )
-      .on('error', (error) => notifier.error(error.message, 'HTML linting error', done));
-
-    return done();
-  };
+      .pipe(htmlhint('.htmlhintrc.json'))
+      .pipe(htmlhint.reporter('htmlhint-stylish'));
 };
